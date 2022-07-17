fc630ad78186fc7e4c54047bffb06975c3e4326e
Alok Choudhary <alok.c83@gmail.com>
2022-07-16 21:16:11 -0500

    added Firebase ask dependency to SPM

---
+++ Sources/Login-with-Apple-Firebase-SwiftUI/Login_with_Apple_Firebase_SwiftUI.swift
@@ -0,0 +1,6 @@
+public struct Login_with_Apple_Firebase_SwiftUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Login-with-Apple-Firebase-SwiftUITests/Login_with_Apple_Firebase_SwiftUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Login_with_Apple_Firebase_SwiftUI
+
+final class Login_with_Apple_Firebase_SwiftUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Login_with_Apple_Firebase_SwiftUI().text, "Hello, World!")
+    }
+}
